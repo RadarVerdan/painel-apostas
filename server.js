a9bebb75c9f5dd94a7c77f8b78f21e599891141a
RadarVerdan <mamuterj3@gmail.com>
2026-02-24 03:06:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Rodando na porta:", PORT);
+});
   console.log("Rodando na porta:", PORT);
 });
