d7222c94c5086dbb817e0ca05d17aa0cdba0c417
RadarVerdan <mamuterj3@gmail.com>
2026-02-23 22:10:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
