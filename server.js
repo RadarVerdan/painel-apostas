fe06e5bd1218a74a9ea8f43247feb45cc3d033aa
RadarVerdan <mamuterj3@gmail.com>
2026-02-24 02:16:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.use(express.static(__dirname));
 
-app.get("/health", (req, res) => res.status(200).send("ok"));
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
